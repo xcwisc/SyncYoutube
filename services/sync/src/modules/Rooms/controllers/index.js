d7369ddb1f40e769eddaa931b3be43a21b636efb
Chang Xu <xuchang@quanweitech.com>
2019-07-22 17:34:46 +0800

    add get rooms route

--- services/sync/src/modules/Rooms/controllers/index.js
+++ services/sync/src/modules/Rooms/controllers/index.js
@@ -1,5 +1,33 @@
+const redis = require('redis');
+
+const client = redis.createClient();
+// const client = redis.createClient(6379, 'redis');
+
 module.exports.getAllRoomsInfo = (req, res) => {
-  res.status(200).json({
-    message: 'ok'
+  client.keys('*', (err, rooms) => {
+    if (err) {
+      res.status(500).json({
+        message: 'fail',
+      })
+    }
+    let data = [];
+    rooms.forEach((room, index) => {
+      client.llen(room, (err, len) => {
+        if (err) {
+          res.status(500).json({
+            message: 'fail',
+          })
+        }
+        let roomInfo = {};
+        roomInfo[room] = len;
+        data.push(roomInfo);
+        if (index === rooms.length - 1) {
+          res.status(200).json({
+            message: 'success',
+            data: data
+          })
+        }
+      })
+    })
   })
 };
