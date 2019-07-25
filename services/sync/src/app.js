5b3fb966aba8c972d2c1cb62f0441a14556cb562
xcwisc <cxu249@wisc.edu>
2019-07-25 10:27:19 +0800

    v1.0

--- services/sync/src/app.js
+++ services/sync/src/app.js
@@ -1,15 +1,17 @@
 const http = require('http');
+// const morgan = require('morgan');
 
 http.globalAgent.maxSockets = 1024;
-
 const app = require('express')();
 
-const server = http.Server(app);
-const io = require('./lib/sockets').listen(server);
-
 const apiRoutes = require('./router');
-
+// app.use(morgan('combined'));
+// app.get('/api/v1', (req, res) => { res.send('hello') });
 app.use('/api/v1', apiRoutes);
+
+const server = http.Server(app);
+
+const io = require('./lib/sockets').listen(server);
 
 server.listen(8080, () => {
   const host = server.address().address;
@@ -17,3 +19,7 @@ server.listen(8080, () => {
 
   console.log("Sync listening at http://%s:%s", host, port);
 });
+
+// app.listen(8080, (err) => {
+//   console.log("Sync listening at %s", 8080);
+// })
